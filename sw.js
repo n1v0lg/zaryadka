b87cd4119fdfbaffba71974fd7ef61279b988049
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2026-09-11 20:35:42 +0200

    Icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Network-first: edits pushed to GitHub show up on next open; cached copy is used offline.
-const CACHE = 'boris-v2';
+const CACHE = 'boris-v3';
 const ASSETS = ['./', './index.html', './workouts.json', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
